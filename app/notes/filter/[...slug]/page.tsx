8ed0ae96ab8c8796d31ba9efddd24153d9fbb95c
tokareva-stacy <tokarevanyura@gmail.com>
2025-10-25 00:17:44 +0300

    some fixes

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,9 +5,27 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import NotesClient from "./Notes.client";
+import { Metadata } from "next";
 
 interface Props {
   params: Promise<{ slug: string[] }>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+
+  return {
+    title: `${slug[0]} Notes`,
+    description: `Notes by category "${slug[0]}"`,
+    openGraph: {
+      title: `Notes: ${slug[0]}`,
+      description: `Notes by category "${slug[0]}"`,
+      url: `https://08-zustand-orcin-six.vercel.app/notes/filter/${slug[0]}`,
+      images: [
+        { url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg" },
+      ],
+    },
+  };
 }
 
 const topic = "";
